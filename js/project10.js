bb368deabfd03d6cc3a91b9d656bb8b9e24899ea
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-07-11 18:15:40 +0900

    Add files via upload

--- js/project10.js
+++ js/project10.js
@@ -51,4 +51,25 @@ $(function () {
     $(this).addClass('on').siblings().removeClass('on');
   });
 
+  $('.tab_link li').on('click', function (event) {
+    event.preventDefault();
+    let idx = $(this).index(); //0,1,2
+
+    $(this).addClass('on')
+      .siblings().removeClass('on');
+
+    //방법 1
+    //$('.tab_content .con').removeClass('on');
+    // $('.tab_content .con').eq(idx).addClass('on')
+
+    //방법 2
+    $('.tab_content .con').eq(idx).addClass('on')
+      .siblings().removeClass('on');
+
+
+    console.log(event, event.target, event.currentTarget, $(this), $(this).index());
+
+
+  });
+
 })
